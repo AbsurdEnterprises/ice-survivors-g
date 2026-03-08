59559835eeccf5d1bda2c58be8b6482818079f16
iamtaylorwest <taylor@absurd.enterprises>
2026-03-08 18:14:20 +0700

    Fix syntax and indentation errors (tab vs spaces mixed)

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -18,11 +18,11 @@ func _process(delta: float) -> void:
             offset = Vector2.ZERO
 
 func _ready() -> void:
-	if not target_node.is_empty():
-		target = get_node(target_node)
-	elif get_tree().has_group("player"):
-		target = get_tree().get_nodes_in_group("player")[0]
+    if not target_node.is_empty():
+        target = get_node(target_node)
+    elif get_tree().has_group("player"):
+        target = get_tree().get_nodes_in_group("player")[0]
 
 func _physics_process(_delta: float) -> void:
-	if is_instance_valid(target) and not is_locked:
-		global_position = global_position.lerp(target.global_position, follow_weight)
+    if is_instance_valid(target) and not is_locked:
+        global_position = global_position.lerp(target.global_position, follow_weight)
